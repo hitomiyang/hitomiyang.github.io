d26c19cf8811d6862419436474b1532df1424760
Hitomi <hitomi.fox@gmail.com>
2024-07-12 22:09:33 +0800

    update README.md with available scripts

--- src/Example.jsx
+++ src/Example.jsx
@@ -2,59 +2,65 @@ import React, { useRef, useState, useEffect } from "react";
 import useHover from "./hooks/useHover";
 import { clsx } from "clsx";
 
-// // Hook
+// Hook
 const useRefHover = () => {
-	const [value, setValue] = useState(false);
+  const [value, setValue] = useState(false);
 
-	const ref = useRef(null);
+  const ref = useRef(null);
 
-	const handleMouseOver = () => {
-		setValue(true);
-	};
-	const handleMouseOut = () => setValue(false);
+  const handleMouseOver = () => {
+    setValue(true);
+  };
+  const handleMouseOut = () => setValue(false);
 
-	useEffect(() => {
-		const node = ref.current;
-		if (node) {
-			node.addEventListener("mouseover", handleMouseOver);
-			node.addEventListener("mouseout", handleMouseOut);
-		}
-		return () => {
-			node.removeEventListener("mouseover", handleMouseOver);
-			node.removeEventListener("mouseout", handleMouseOut);
-		};
-	});
+  useEffect(() => {
+    const node = ref.current;
+    if (node) {
+      node.addEventListener("mouseover", handleMouseOver);
+      node.addEventListener("mouseout", handleMouseOut);
+    }
+    return () => {
+      node.removeEventListener("mouseover", handleMouseOver);
+      node.removeEventListener("mouseout", handleMouseOut);
+    };
+  });
 
-	return [ref, value];
+  return [ref, value];
 };
 
 function Example() {
-	const renderElement = (hovered) => {
-		return <div className={hovered ? "yes" : "NO"}>{hovered ? "yes" : "NO"}</div>;
-	};
-	const renderElement2 = (hovered) => {
-		return <div className={clsx("NO cursor-pointer", { hovered: "yes" })}>{hovered ? "yes" : "NO"}</div>;
-	};
+  const renderElement = (hovered) => {
+    return (
+      <div className={hovered ? "yes" : "NO"}>{hovered ? "yes" : "NO"}</div>
+    );
+  };
+  const renderElement2 = (hovered) => {
+    return (
+      <div className={clsx("NO cursor-pointer", { hovered: "yes" })}>
+        {hovered ? "yes" : "NO"}
+      </div>
+    );
+  };
 
-	const [firstElementRef, isFirstElementHover] = useRefHover();
-	const [secondElement, isSecondElementHover] = useHover(renderElement);
-	const [thirdElement, isThirdElementHover] = useHover(renderElement2);
-	return (
-		<div>
-			{/*useRefHover*/}
-			<div ref={firstElementRef} className={isFirstElementHover ? "yes" : "NO"}>
-				{isFirstElementHover ? "yes" : "NO"}
-			</div>
-			<p>{`first: ${String(isFirstElementHover)}`}</p>
+  const [firstElementRef, isFirstElementHover] = useRefHover();
+  const [secondElement, isSecondElementHover] = useHover(renderElement);
+  const [thirdElement, isThirdElementHover] = useHover(renderElement2);
+  return (
+    <div>
+      {/*useRefHover*/}
+      <div ref={firstElementRef} className={isFirstElementHover ? "yes" : "NO"}>
+        {isFirstElementHover ? "yes" : "NO"}
+      </div>
+      <p>{`first: ${String(isFirstElementHover)}`}</p>
 
-			{/*useHover*/}
-			{secondElement}
-			<p>{`second: ${String(isSecondElementHover)}`}</p>
+      {/*useHover*/}
+      {secondElement}
+      <p>{`second: ${String(isSecondElementHover)}`}</p>
 
-			{thirdElement}
-			<p>{`third: ${String(isThirdElementHover)}`}</p>
-		</div>
-	);
+      {thirdElement}
+      <p>{`third: ${String(isThirdElementHover)}`}</p>
+    </div>
+  );
 }
 
 export default Example;
